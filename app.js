176c49f95f4b0d01cf8d26d1ee2b594278f401ef
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-05-26 08:20:00 +0530

    set up index blog page 274

--- app.js
+++ app.js
@@ -19,19 +19,32 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-Blog.create({
-  title: "Test Blog",
-  image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-  body: "Hello this is a blog post"
-},function(err, createdBlog ){
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(createdBlog);
-  }
-});
+// Blog.create({
+//   title: "Test Blog",
+//   image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+//   body: "Hello this is a blog post"
+// },function(err, createdBlog ){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(createdBlog);
+//   }
+// });
 
 //RESTFUL ROUTES
+app.get("/", function(req,res){
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+  Blog.find({}, function(err, blogs){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {blogs: blogs});
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("server has started on port 300");
