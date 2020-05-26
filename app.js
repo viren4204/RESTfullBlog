34871ba639adb6c8511c628986f872f032594fee
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-05-26 07:46:12 +0530

    add hardcore database to mongoose via app.js

--- app.js
+++ app.js
@@ -17,7 +17,19 @@ var blogSchema = new mongoose.Schema({
   created: {type: Date, default: Date.now  }
 });
 
-var Blog = mongoose.model("Blog", blogSchema)
+var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+  title: "Test Blog",
+  image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+  body: "Hello this is a blog post"
+},function(err, createdBlog ){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(createdBlog);
+  }
+});
 
 //RESTFUL ROUTES
 
