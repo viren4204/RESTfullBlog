e2ac16e388b8bd457d0cdebe5b0bb4d7abdbddd4
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-05-28 08:18:00 +0530

    create update route

--- app.js
+++ app.js
@@ -90,7 +90,13 @@ app.get("/blogs/:id/edit", function(req, res){
 
 //UPDATE ROUTES
 app.put("/blogs/:id", function(req, res){
-  res.send("UPDATE ROUTE");
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if (err) {
+      res.redirect("/blogs");
+    }else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
 });
 
 app.listen(3000, function(){
