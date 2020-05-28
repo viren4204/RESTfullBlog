01440be12c5a65240442f46879a13dc43f6e52d1
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-05-28 07:53:52 +0530

    create edit route

--- app.js
+++ app.js
@@ -75,6 +75,17 @@ app.get("/blogs/:id", function(req,res){
   });
 });
 
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log("server has started on port 300");
 });
